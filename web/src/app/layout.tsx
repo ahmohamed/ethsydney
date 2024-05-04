3f7eacd6f481a158955ffc48cc1b287b7670c422
Fabian Ferno <fabianferno@gmail.com>
2024-05-04 20:36:04 +0530

    Add routes to sign + contract config

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,7 +7,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Geo Sync",
+  title: "Connekt",
   description: "This app does something",
   icons: ["giphy.gif"],
 };
